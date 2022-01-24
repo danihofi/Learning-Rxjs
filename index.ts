35b519a099d5678d4603896056031b666f134a84
danihofi <danihofi@web.de>
2022-01-24 18:44:10 +0100

    add rxjs fromEvent

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import './style.css';
 
-import { of, map, take, Observable } from 'rxjs';
+import { of, map, take, fromEvent, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 of('World', 'Test')
@@ -9,5 +9,8 @@ of('World', 'Test')
     tap(console.log),
     map((name) => `Hello, ${name}!`))
   .subscribe(console.log);
+
+
+fromEvent(document,'click').subscribe(() => console.log('Clicked'));
 
 // Open the console in the bottom right to see results.
